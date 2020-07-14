df3ec80f2a717224df323cd8b9102c870da3b1ca
liyi <li.yi@chinaott.net>
2020-07-14 17:30:08 +0800

    Revert "lombok 支持2"
    This reverts commit 2fbac17

--- mboog-generator/src/main/java/mboog/generator/plugins/ModelLombokPlugin.java
+++ mboog-generator/src/main/java/mboog/generator/plugins/ModelLombokPlugin.java
@@ -16,14 +16,14 @@ public class ModelLombokPlugin extends PluginAdapter {
 
     @Override
     public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        modelLombokGenerated(topLevelClass, false, introspectedTable.getPrimaryKeyColumns().size() > 1);
+        modelLombokGenerated(topLevelClass);
         return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
     }
 
 
     @Override
     public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        modelLombokGenerated(topLevelClass, true, false);
+        modelLombokGenerated(topLevelClass);
         return super.modelPrimaryKeyClassGenerated(topLevelClass, introspectedTable);
     }
 
@@ -31,43 +31,32 @@ public class ModelLombokPlugin extends PluginAdapter {
     /**
      * Model 类 lombok 处理
      *
-     * @param topLevelClass   Top level Class
-     * @param primaryKeyClass Primary key Class
+     * @param topLevelClass
      */
-    private void modelLombokGenerated(TopLevelClass topLevelClass, boolean primaryKeyClass, boolean callSuper) {
-
+    private void modelLombokGenerated(TopLevelClass topLevelClass) {
         topLevelClass.addImportedType("lombok.Getter");
         topLevelClass.addImportedType("lombok.Setter");
-        topLevelClass.addImportedType("lombok.ToString");
         topLevelClass.addImportedType("lombok.EqualsAndHashCode");
+        topLevelClass.addImportedType("lombok.ToString");
 
         topLevelClass.addAnnotation("@Getter");
         topLevelClass.addAnnotation("@Setter");
+        topLevelClass.addAnnotation("@EqualsAndHashCode");
+        topLevelClass.addAnnotation("@ToString");
 
-        if (primaryKeyClass || !callSuper) {
-            topLevelClass.addAnnotation("@ToString");
-            topLevelClass.addAnnotation("@EqualsAndHashCode");
-        } else {
-            topLevelClass.addAnnotation("@ToString(callSuper = true)");
-            topLevelClass.addAnnotation("@EqualsAndHashCode(callSuper = true)");
-        }
-
-        // Remove methods [get*, set*, toString, equals, hashCode]
+        // Remove methods [get*, set*, hashCode, toString]
         List<Method> methods = topLevelClass.getMethods();
         for (int i = 0; i < methods.size(); i++) {
             Method method = methods.get(i);
             if (method.getName().startsWith("get")
                     || method.getName().startsWith("set")
-                    || method.getName().matches("toString|equals|hashCode")) {
-                methods.remove(i--);
+                    || method.getName().matches("toString|hashCode")) {
+                methods.remove(i);
+                i--;
             }
         }
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 
     @Override
     public boolean validate(List<String> warnings) {
